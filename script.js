b1f8fe2a51f8a67a1dd4157693468fcc82964d79
Rishab-kumar-026 <rks2034287@gmail.com>
2024-07-25 14:53:14 +0530

    Find Capital.in

---
+++ script.js
@@ -0,0 +1,20 @@
+const nav_dialogue = document.getElementById("navigation");
+
+function handle(){
+    nav_dialogue.classList.toggle('hidden');
+};
+
+const box_divs = document.querySelectorAll(".box");
+
+box_divs.forEach((box_div) => {
+    box_div.addEventListener("mouseover", () => {
+        box_div.style.backgroundColor = "#FFDEE9";
+        box_div.style.backgroundImage = "linear-gradient(0deg, #FFDEE9 0%, #B5FFFC 100%)";
+        box_div.style.backgroundSize = "cover";
+        box_div.style.backgroundRepeat = "no-repeat";
+    });
+
+    box_div.addEventListener("mouseout", () => {
+        box_div.style.background = "#FBFBFD";
+    });
+});
