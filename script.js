5c332c0e6d6c038a726430107361c3046a2156e6
Rishab-kumar-026 <rks2034287@gmail.com>
2024-07-27 11:11:14 +0530

    Investor Portal Introduced

--- script.js
+++ script.js
@@ -18,3 +18,26 @@ box_divs.forEach((box_div) => {
         box_div.style.background = "#FBFBFD";
     });
 });
+
+
+const links = document.querySelectorAll(".nav-links-mb a");
+const div = document.getElementById("navigation");
+
+links.forEach((link) =>{
+    link.addEventListener("click",()=>{
+       div.classList.add("hidden");
+    });
+  
+});
+
+const investor_box = document.querySelectorAll(".detail-box h1");
+const details = document.querySelectorAll(".more-details");
+const investor_icon = document.querySelectorAll(".detail-box h1 i");
+
+investor_box.forEach((investor, index) => {
+    investor.addEventListener("click", () => {
+        details[index].classList.toggle("hidden");
+        investor_icon[index].classList.toggle("fa-xmark");
+    });
+});
+
